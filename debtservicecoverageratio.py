3ebf74b7d5df34c50478dedd33f8fea5390a64d9
vamsi-4-3-4 <94716145+vamsi-4-3-4@users.noreply.github.com>
2022-08-05 17:24:28 +0530

    Update debtservicecoverageratio.py

--- debtservicecoverageratio.py
+++ debtservicecoverageratio.py
@@ -4,7 +4,6 @@ class financial:
 add=financial()
 demodscr=add.dscr(40000000,14800000)
 print("DEMO DEBT SERVICE COVERAGE RATIO:",demodscr)
-
 ebitda=float(input("ENTER YOUR EBITDA:"))
 totaldebtservice=float(input("ENTER YOUR TOTAL DEBT SERVICE:"))
 user=add.dscr(ebitda,totaldebtservice)
